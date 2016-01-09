94c3b8afc40a7e3313f659e61c7bb381c58be8ed
Mark De Ruyter <mderu001@ucr.edu>
2016-01-08 18:13:49 -0800

    Ignored bloat.mark. Removed comments
    Real coders don't need comments.

--- main.cpp
+++ main.cpp
@@ -9,9 +9,7 @@ for (int i = 0; i < 100; i++)
 	cout << i; 
 	if (i % 3 == 0 || i % 5 == 0)
 	{
-		//This line outputs Fizz whenever the number is divisible by 3.
 		if (i % 3 == 0) { cout << "Fizz"; }
-		//This line outputs Buzz whenever the number is divisible by 5.
 		if (i % 5 == 0) { cout << "Buzz"; }
 	}
 		cout << endl; 
